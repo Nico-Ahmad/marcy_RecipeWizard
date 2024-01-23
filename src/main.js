303a75ec8cea1781e46b349c1bb493bc4671c2fe
ahmad <ahmadhamza052005@gmail.com>
2024-01-23 12:48:12 -0500

    attemptOneFetch

--- src/main.js
+++ src/main.js
@@ -1,11 +1,15 @@
-const FetchMealByIngredient = 
+const FetchMealByIngredient = async (userInput) => {
+  try {
+    const response = await fetch(
+      `www.themealdb.com/api/json/v1/1/filter.php?i=chicken_breast`
+    );
+    const data = await response.json();
+    console.log('heres the data', data)
+    if (!response.ok) throw Error('fetch failed Status:', response.status)
+  } catch (error) {
+    console.error('caught error', error)
+  }
+};
 
-
-
-
-
-
-const main = () => {
-
-} 
+const main = () => {};
 main();
