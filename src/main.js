df695341675620ff2022436876528ab988e9e894
ahmad <ahmadhamza052005@gmail.com>
2024-01-23 12:52:59 -0500

    push

--- src/main.js
+++ src/main.js
@@ -3,12 +3,12 @@ const FetchMealByIngredient = async (userInput) => {
     const response = await fetch(
       `www.themealdb.com/api/json/v1/1/filter.php?i=chicken_breast`
     );
-    const data = await response.json();
-    console.log('heres the data', data)
+    const meals = await response.json();
+    console.log('heres the data', meals)
     if (!response.ok) throw Error('fetch failed Status:', response.status)
   } catch (error) {
     console.error('caught error', error)
-  }
+  } 
 };
 
 const main = () => {};
