9518ce6e2f36cba8e1f5c8f4a422e751119247e3
tailsmster <lotailsmonsieur62c900@gmail.com>
2024-01-23 14:10:18 -0500

    push

--- src/main.js
+++ src/main.js
@@ -1,15 +1,19 @@
-const fetchMealByIngredient = async (userInput) => {
+const fetchMealsByIngredient = async (userInput) => {
   try {
     const response = await fetch(
       `https://https://www.themealdb.com/api/json/v1/1/filter.php?i=${userInput}`
     );
+    if (!response.ok) throw Error('fetch failed Status:', response.status)
+
     const meals = await response.json();
     console.log('heres the data', meals)
-    if (!response.ok) throw Error('fetch failed Status:', response.status)
+    return meals
   } catch (error) {
     console.error('caught error', error)
   } 
 };
+
+// const fetchMealDetails
 
 
 const fetchTest = async () => {
@@ -25,6 +29,8 @@ const fetchTest = async () => {
     console.error('caught error', error)
   }
 }
+
+const fetch
 
 const main = () => {
   fetchTest()
