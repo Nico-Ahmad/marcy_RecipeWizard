e1d172076a49d0074474c214056f19b33374abbb
ahmad <ahmadhamza052005@gmail.com>
2024-01-24 06:10:09 -0500

    universalFetchesCONNECTEDandFormInputConverter

--- src/dom-utils.js
+++ src/dom-utils.js
@@ -1,1 +1,15 @@
 // temp
+export const handleSubmit = (e) => {
+    e.preventDefault();
+
+  // get form data
+  const form = e.target;
+  const formData = new FormData(form);
+  const formObj = Object.fromEntries(formData);
+
+  console.log("here is your data:", formObj.ingredientUserInput);
+
+ 
+  form.reset();
+
+}
--- src/main.js
+++ src/main.js
@@ -1,49 +1,62 @@
-const fetchMealsByIngredient = async (userInput) => {
-  try {
-    const response = await fetch(
-      `https://https://www.themealdb.com/api/json/v1/1/filter.php?i=${userInput}`
-    );
-    if (!response.ok) throw Error('fetch failed Status:', response.status)
+import { handleSubmit } from "./dom-utils";
 
-    const meals = await response.json();
-    console.log('heres the data', meals)
-    return meals
-  } catch (error) {
-    console.error('caught error', error)
-  } 
-};
-
-// const fetchMealDetails
-
-
-const fetchTest = async () => {
-  try {
-    const response = await fetch(`https://www.themealdb.com/api/json/v1/1/lookup.php?i=52772`);
-    if (!response.ok) throw Error('fetch failed Status:', response.status)
-
-    const data = await response.json();
-
-    console.log(data)
-
-  } catch {
-    console.error('caught error', error)
-  }
-}
-
-const fetcher = async (url) => {
+export const fetcher = async (url) => {
   try {
     const response = await fetch(url);
-    if (!response.ok) throw Error('fetch failed Status:', response.status)
+    if (!response.ok) throw new Error('Fetch failed with status:', response.status);
 
     const data = await response.json();
+    console.log('the meal data', data)
+    if (data.meals && data.meals.length > 0) {
+      const theMeal = data.meals[0];  
+      
+      // object to holds meal information
+      let mealInfo = {};
 
-    console.log(data)
+      // filter out properties from first URL 
+      if (theMeal.strMealThumb && theMeal.idMeal) {
+        mealInfo.image = theMeal.strMealThumb;
+        mealInfo.id = theMeal.idMeal;
+      }
+
+      // filter out properties from second URL 
+      if (theMeal.strMeal && theMeal.strArea && theMeal.strInstructions && theMeal.strYoutube) {
+        mealInfo.title = theMeal.strMeal;
+        mealInfo.origin = theMeal.strArea;
+        mealInfo.instructions = theMeal.strInstructions;
+        mealInfo.youtubeUrl = theMeal.strYoutube;
+      }
+        //console.log(mealInfo)
+      return mealInfo;
+    } else {
+      // if no array 
+      return null 
+    }
   } catch (error) {
-    console.error('caught error', error);
+    console.error('caught error:', error);
+    // fetch fails  
+    return null
   }
-}
+};
 
-const main = () => {
-  fetchTest()
+const main =  async () => {
+  // form 
+
+  const form = document.querySelector('#meal-form')
+  form.addEventListener('submit',handleSubmit)
+  const mealByIngUrl = `https://www.themealdb.com/api/json/v1/1/filter.php?i=tomato`
+  // returns img and id 
+  
+  // returns all info 
+
+  // mealById === www.themealdb.com/api/json/v1/1/lookup.php?i=52772
+  // mealBy Ingredient === www.themealdb.com/api/json/v1/1/filter.php?i=chicken_breast
+  const mealByIng = await fetcher(mealByIngUrl);
+  const mealInfoUrl = `https://www.themealdb.com/api/json/v1/1/lookup.php?i=${mealByIng.id}`
+  console.log('Meal by Ingredient:', mealByIng);
+
+  const mealInfo = await fetcher(mealInfoUrl);
+  console.log('Meal Information:', mealInfo);
+  
 };
 main();
