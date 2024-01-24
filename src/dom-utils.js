52c4a6c08f18ec9682060db57dcce13281387ada
ahmad <ahmadhamza052005@gmail.com>
2024-01-24 12:11:07 -0500

    pushBuild

--- src/dom-utils.js
+++ src/dom-utils.js
@@ -5,9 +5,9 @@ export const handleSubmit = (e) => {
   const form = e.target;
   const formData = new FormData(form);
   const formObj = Object.fromEntries(formData);
-
+  const userIngredients = formObj.ingredientUserInput
   console.log("here is your data:", formObj.ingredientUserInput);
 
   form.reset();
-  return formObj;
+  return userIngredients;
 };
