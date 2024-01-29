d9556d839aca402c16b6ba17f4290c4a1d3b3981
tailsmster <lotailsmonsieur62c900@gmail.com>
2024-01-29 09:02:53 -0500

    r

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ export default defineConfig({
   // GitHub Pages expects an index.html in the root directory
   // so just run npm build before pushing to GitHub and this will rebuild our assets to the root
   build: {
-    outDir: 'dist', // Change this to 'dist' or similar
+    outDir: '..', // Change this to 'dist' or similar
   },
   // needed for github pages just put the repo name here
   base: '/marcy_RecipeWizard/', 
