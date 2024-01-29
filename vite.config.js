40c2b9baa088cfb9d0e4cab809cf8cf7a4ecf3f5
ahmad <ahmadhamza052005@gmail.com>
2024-01-29 08:34:13 -0500

    viteRender

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,9 @@ import { defineConfig } from 'vite'
 export default defineConfig({
   // GitHub Pages expects an index.html in the root directory
   // so just run npm build before pushing to GitHub and this will rebuild our assets to the root
-  build: { outDir: '..' },
+  build: {
+    outDir: 'dist', // Change this to 'dist' or similar
+  },
   // needed for github pages just put the repo name here
   base: '/marcy_RecipeWizard/', 
 });
